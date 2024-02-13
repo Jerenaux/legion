dddb45cbf4d1c099b8f7718a1f8ac859036a2e29
Jerome <jerome.renaux@gmail.com>
2024-02-13 12:47:09 +0100

    Avatar area

--- client/src/components/TopMenuBar.tsx
+++ client/src/components/TopMenuBar.tsx
@@ -3,18 +3,24 @@ import { h } from 'preact';
 
 const TopMenuBar = () => (
   <div className="topMenu">
+
     <div className="leftSection">
-      <div className="avatar">
+      <div className="profileArea">
+        <div className="avatarBg"></div>
+        <div className="profileBg">
+            <div className="avatarName">AVATAR NAME</div>
+            <div className="level">Lv.13</div>
+        </div>
       </div>
-      <div className="avatarName">AVATAR NAME</div>
-      <div className="level">Lv.13</div>
     </div>
+
     <div className="centerSection">
       <button className="playButton">PLAY</button>
       <button className="teamButton">TEAM</button>
       <button className="shopButton">SHOP</button>
       <button className="rankButton">RANK</button>
     </div>
+
     <div className="rightSection">
       <div className="currency">
         3.000.000
