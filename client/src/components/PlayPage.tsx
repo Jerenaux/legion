0a2be3c4da91f30edbaec2f27b390358b5d57e50
Jerome <jerome.renaux@gmail.com>
2024-02-23 19:59:21 +0100

    Add mock data for play page

--- client/src/components/PlayPage.tsx
+++ client/src/components/PlayPage.tsx
@@ -7,6 +7,70 @@ import Button from './Button';
 class PlayPage extends Component {
   render() {
 
+    const data = {
+      dailyQuests: [
+        {
+          name: "Use 5 fire spells",
+          rewards: {gold: 500, xp: 2000},
+          completion: 0.8
+        },
+        {
+          name: "Win 3 games",
+          rewards: {gold: 700, xp: 1000},
+          completion: 1 // = completed
+        },
+        {
+          name: "Use 5 fire spells",
+          rewards: {gold: 500, xp: 2000},
+          completion: 0.8
+        },
+      ],
+      ongoingGames: [
+        {
+          teamA: {
+            name: "TeamA",
+            teamSize: 6,
+            aliveCharacters: 3,
+          },
+          teamB: {
+            name: "TeamB",
+            teamSize: 6,
+            aliveCharacters: 5,
+          },
+          spectators: 5,
+          duration: 900, // seconds
+        },
+        {
+          teamA: {
+            name: "TeamA",
+            teamSize: 6,
+            aliveCharacters: 3,
+          },
+          teamB: {
+            name: "TeamB",
+            teamSize: 6,
+            aliveCharacters: 5,
+          },
+          spectators: 5,
+          duration: 900, // seconds
+        },
+        {
+          teamA: {
+            name: "TeamA",
+            teamSize: 6,
+            aliveCharacters: 3,
+          },
+          teamB: {
+            name: "TeamB",
+            teamSize: 6,
+            aliveCharacters: 5,
+          },
+          spectators: 5,
+          duration: 900, // seconds
+        }
+      ]
+    }
+
     return (
         <div className="play-content">
           <Roster />
--- client/src/components/Roster.tsx
+++ client/src/components/Roster.tsx
@@ -16,7 +16,78 @@ class Roster extends Component<object, RosterState> {
 
   async fetchRosterData() {
     try {
-        const data = await apiFetch('rosterData');
+        // const data = await apiFetch('rosterData');
+        // List 3 characters; in practice a player can own between 3 and 10 characters
+        const data = {
+          "characters": [
+            {
+              "id": "moPHy7JuKxvKcamrfRvM",
+              "skills": [],
+              "skill_slots": 0,
+              "level": 1,
+              "stats": {
+                "spdef": 8,
+                "mp": 20,
+                "spatk": 7,
+                "def": 11,
+                "hp": 100,
+                "atk": 11
+              },
+              "carrying_capacity": 3,
+              "name": "resident_jade",
+              "xp": 0,
+              "inventory": [],
+              "portrait": "7_4", // Maps to file path like this: backgroundImage: `url(/sprites/${portrait}.png)`,
+              "class": 0 // Can be mapped to string representation of class using `classEnumToString` function
+            },
+            {
+              "id": "SnRpBhO0uad6qF4SlIZI",
+              "skills": [
+                1
+              ],
+              "skill_slots": 3,
+              "level": 1,
+              "stats": {
+                "spdef": 8,
+                "mp": 30,
+                "spatk": 11,
+                "def": 7,
+                "hp": 80,
+                "atk": 7
+              },
+              "carrying_capacity": 3,
+              "name": "working_chocolate",
+              "xp": 0,
+              "inventory": [],
+              "portrait": "2_8",
+              "class": 1
+            },
+            {
+              "id": "WqYLnK7UwdZrug56DjuN",
+              "skills": [
+                0,
+                2,
+                3
+              ],
+              "skill_slots": 3,
+              "level": 1,
+              "stats": {
+                "spdef": 9,
+                "mp": 40,
+                "spatk": 10,
+                "def": 8,
+                "hp": 80,
+                "atk": 6
+              },
+              "carrying_capacity": 3,
+              "name": "dual_silver",
+              "xp": 0,
+              "inventory": [],
+              "portrait": "3_2",
+              "class": 2
+            }
+          ]
+        };
         console.log(data);
         this.setState({ 
           characters: data.characters
