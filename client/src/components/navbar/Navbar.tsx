8abfaf4e4e8c9996f8b841dac24188249adbc27c
Jerome <jerome.renaux@gmail.com>
2024-08-22 15:38:00 +0200

    Navbar assets

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -18,6 +18,13 @@ import playActiveIcon from '@assets/play_btn_active.png';
 import teamActiveIcon from '@assets/team_btn_active.png';
 import shopActiveIcon from '@assets/shop_btn_active.png';
 import rankActiveIcon from '@assets/rank_btn_active.png';
+import expandBtn from '@assets/expand_btn.png';
+import helpIcon from '@assets/svg/help.svg';
+import xIcon from '@assets/svg/x.svg';
+import discordIcon from '@assets/svg/discord.svg';
+import copyIcon from '@assets/svg/copy.svg';
+import logoutIcon from '@assets/svg/logout.svg';
+
 const avatarContext = require.context('@assets/avatars', false, /\.(png|jpe?g|svg)$/);
 
 enum MenuItems {
@@ -85,8 +92,7 @@ class Navbar extends Component<Props, State> {
         });
     };
 
-    formatNumber= (number) => {
-        // const format = 'fr-FR';
+    formatNumber = (number) => {
         const format = 'en-US';
         return new Intl.NumberFormat(format, { 
           useGrouping: true,
@@ -112,7 +118,7 @@ class Navbar extends Component<Props, State> {
                 <div className="flexContainer">
                     <div className="logoContainer">
                         <Link href="/play" className="gameLogo">
-                            <img src={legionLogo} />
+                            <img src={legionLogo} alt="Legion Logo" />
                         </Link>
                     </div>
                     <div className="avatarContainer">
@@ -127,22 +133,22 @@ class Navbar extends Component<Props, State> {
                 <div className="menuItems">
                     <Link href="/play" onMouseOver={() => this.setState({ hovered: MenuItems.PLAY })} onMouseLeave={() => this.setState({ hovered: '' })}>
                         <div className={`menuItemContainer ${currentPage(Routes.PLAY) ? 'activeFlag' : ''}`}>
-                            <img className="menuItem" src={this.state.hovered === MenuItems.PLAY ? playActiveIcon : playIcon} />
+                            <img className="menuItem" src={this.state.hovered === MenuItems.PLAY ? playActiveIcon : playIcon} alt="Play" />
                         </div>
                     </Link>
                     <Link href="/team" onMouseOver={() => this.setState({ hovered: MenuItems.TEAM })} onMouseLeave={() => this.setState({ hovered: '' })}>
                         <div className={`menuItemContainer ${currentPage(Routes.TEAM) ? 'activeFlag' : ''}`}>
-                            <img className="menuItem" src={this.state.hovered === MenuItems.TEAM ? teamActiveIcon : teamIcon} />
+                            <img className="menuItem" src={this.state.hovered === MenuItems.TEAM ? teamActiveIcon : teamIcon} alt="Team" />
                         </div>
                     </Link>
                     <Link href="/shop" onMouseOver={() => this.setState({ hovered: MenuItems.SHOP })} onMouseLeave={() => this.setState({ hovered: '' })}>
                         <div className={`menuItemContainer ${currentPage(Routes.SHOP) ? 'activeFlag' : ''}`}>
-                            <img className="menuItem" src={this.state.hovered === MenuItems.SHOP ? shopActiveIcon : shopIcon} />
+                            <img className="menuItem" src={this.state.hovered === MenuItems.SHOP ? shopActiveIcon : shopIcon} alt="Shop" />
                         </div>
                     </Link>
                     <Link href="/rank" onMouseOver={() => this.setState({ hovered: MenuItems.RANK })} onMouseLeave={() => this.setState({ hovered: '' })}>
                         <div className={`menuItemContainer ${currentPage(Routes.RANK) ? 'activeFlag' : ''}`}>
-                            <img className="menuItem" src={this.state.hovered === MenuItems.RANK ? rankActiveIcon : rankIcon} />
+                            <img className="menuItem" src={this.state.hovered === MenuItems.RANK ? rankActiveIcon : rankIcon} alt="Rank" />
                         </div>
                     </Link>
                 </div>
@@ -150,22 +156,22 @@ class Navbar extends Component<Props, State> {
                 <div className="flexContainer" id="goldEloArea">
                     <UserInfoBar label={`${this.formatNumber(Math.round(this.props.playerData?.gold))}`}  />
                     <UserInfoBar label={`#${this.props.playerData?.rank}`} elo={this.props.playerData?.elo || 1} league={this.props.playerData?.league} />
-                    <div class="expand_btn" style={{backgroundImage: 'url("/expand_btn.png")'}} onClick={() => this.setState({ openDropdown: !this.state.openDropdown })} onMouseEnter={() => this.setState({ openDropdown: true })}>
-                        <div class="dropdown-content" style={dropdownContentStyle} onMouseLeave={() => this.setState({ openDropdown: false })}>
-                            <div className="" onClick={() => window.open('', '_blank')}>
-                                <img src="svg/help.svg" /> How to play
+                    <div className="expand_btn" style={{backgroundImage: `url(${expandBtn})`}} onClick={() => this.setState({ openDropdown: !this.state.openDropdown })} onMouseEnter={() => this.setState({ openDropdown: true })}>
+                        <div className="dropdown-content" style={dropdownContentStyle} onMouseLeave={() => this.setState({ openDropdown: false })}>
+                            <div onClick={() => window.open('', '_blank')}>
+                                <img src={helpIcon} alt="Help" /> How to play
                             </div>
-                            <div className=""  onClick={() => window.open('https://x.com/iolegion', '_blank')}>
-                                <img src="svg/x.svg" /> X.com
+                            <div onClick={() => window.open('https://x.com/iolegion', '_blank')}>
+                                <img src={xIcon} alt="X.com" /> X.com
                             </div>
-                            <div className="" onClick={() => window.open('', '_blank')}>
-                                <img src="svg/discord.svg" /> Discord
+                            <div onClick={() => window.open('', '_blank')}>
+                                <img src={discordIcon} alt="Discord" /> Discord
                             </div>
-                            <div className="" onClick={this.copyIDtoClipboard}>
-                                <img src="svg/copy.svg" /> Player ID
+                            <div onClick={this.copyIDtoClipboard}>
+                                <img src={copyIcon} alt="Copy" /> Player ID
                             </div>
-                            <div className="" onClick={this.props.logout}>
-                                <img src="svg/logout.svg" /> Log out
+                            <div onClick={this.props.logout}>
+                                <img src={logoutIcon} alt="Logout" /> Log out
                             </div>
                         </div>
                     </div>
