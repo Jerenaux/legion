aa169ad9d8ce6c131a83efdd0256d4d93f79693b
Jerome <jerome.renaux@gmail.com>
2024-08-23 15:53:30 +0200

    Working production Docker image for matchmaker

--- deploy_matchmaker.sh
+++ deploy_matchmaker.sh
@@ -4,12 +4,15 @@ SERVICE_NAME="matchmaker"
 IMAGE="gcr.io/legion-32c6d/$SERVICE_NAME"
 TAG="latest"
 REGION="us-central1"
+DEBUG_FLAGS="--no-cache --progress=plain"
 
-docker build -f matchmaker/Dockerfile -t $IMAGE --platform linux/amd64 .
-docker push $IMAGE
+# docker build $DEBUG_FLAGS -f matchmaker/Dockerfile.prod -t $IMAGE --platform linux/amd64 .
+docker build -f matchmaker/Dockerfile.prod -t $IMAGE --platform linux/amd64 .
 
-gcloud run deploy $SERVICE_NAME \
-  --image $IMAGE \
-  --platform managed \
-  --region $REGION \
-  --allow-unauthenticated
+# docker push $IMAGE
+
+# gcloud run deploy $SERVICE_NAME \
+#   --image $IMAGE \
+#   --platform managed \
+#   --region $REGION \
+#   --allow-unauthenticated
