29d5970d7b9ec3c1b0b9d3894f602df03cd8ea90
Jerome <jerome.renaux@gmail.com>
2026-09-04 17:23:08 +0200

    Fix Electron exit when the window closes

--- client/electron.js
+++ client/electron.js
@@ -118,7 +118,7 @@ app.on("second-instance", () => {
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+  app.quit();
 });
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
